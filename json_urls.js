02f1228c68553890a9f712a06785f9127ba26a50
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2026-01-30 17:37:26 +0100

    Update JSON URLs to reflect new lens logic repository structure

--- json_urls.js
+++ json_urls.js
@@ -1,16 +1,16 @@
 module.exports = [
-    "https://raw.githubusercontent.com/Gravitate-Health/gh-test-lab-lens/refs/heads/main/lab-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/pregnancy-lens/refs/heads/main/pregnancy-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/rmm-lens/refs/heads/main/rmm-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/allergy-and-intollerance-lens/refs/heads/main/allergy-and-intollerance-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/interaction-lens/refs/heads/main/interaction-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/diabetes-lens/refs/heads/main/diabetes-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/gh-contact-lens/refs/heads/main/contact-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/gh-questionnaire-lens/refs/heads/main/questionnaire-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/gh-doping-lens/refs/heads/main/doping-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/gh-med-lab-lens/refs/heads/main/qt-prolongation-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-test-lab/refs/heads/main/lab-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-pregnancy/refs/heads/main/pregnancy-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-risk-minimization-material/refs/heads/main/rmm-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-allergy-and-intollerance/refs/heads/main/allergy-and-intollerance-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-interaction/refs/heads/main/interaction-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-diabetes/refs/heads/main/diabetes-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-contact/refs/heads/main/contact-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-questionnaire/refs/heads/main/questionnaire-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-doping/refs/heads/main/doping-lens.json",
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-med-lab/refs/heads/main/qt-prolongation-lens.json",
     "https://raw.githubusercontent.com/joofio/gh-preadmission-lens/refs/heads/main/preadmission-lens.json",
     "https://raw.githubusercontent.com/joofio/gh-community-lens/refs/heads/main/community-lens.json",
-    "https://raw.githubusercontent.com/Gravitate-Health/conditions-lens/refs/heads/main/conditions-lens.json"
+    "https://raw.githubusercontent.com/Gravitate-Health/lens-logic-conditions/refs/heads/main/conditions-lens.json"
     
 ];
